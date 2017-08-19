24180dcc5aac9797554b14869c271b184e47dd7a
k-samuel <kirill.a.egorov@gmail.com>
2017-08-19 13:18:17 +0300

    Designer project update

--- configs/layouts/system/filestorage.designer.dat.files/__config.php
+++ configs/layouts/system/filestorage.designer.dat.files/__config.php
@@ -3,7 +3,7 @@
   'runnamespace' => 'appFilestorageRun',
   'files' => 
   array (
-    0 => '/resources/dvelum-filestorage/js/FilestorageUploadWindow.js',
+    0 => '/resources/dvelum_filestorage/js/FilestorageUploadWindow.js',
   ),
   'langs' => 
   array (
