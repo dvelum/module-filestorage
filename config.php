02f128749280a04c758a7597e863a171fd5ac8e8
Kirill Yegorov <kirill.a.egorov@gmail.com>
2020-11-29 12:16:27 +0300

    Composer 2.0 support added

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'id' => 'dvelum-module-filestorage',
-    'version' => '3.0.0',
+    'version' => '3.0.1',
     'author' => 'Kirill Yegorov',
     'name' => 'DVelum File Storage',
     'configs' => './configs',
